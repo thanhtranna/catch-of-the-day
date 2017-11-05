f7c734f9c5b4c04d1ac4d070d42f3b654a426c4e
Tran Thanh <thanhtv.itedu@gmail.com>
2017-11-05 14:36:33 +0700

    thanhtv initial project

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC6a-CmTRJ2lRT3jeJle1XdHCZwxRQoeTQ",
+	authDomain: "catch-of-the-day-tran-thanh.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-tran-thanh.firebaseio.com",
+  projectId: "catch-of-the-day-tran-thanh",
+  storageBucket: "catch-of-the-day-tran-thanh.appspot.com",
+  messagingSenderId: "442462963767"
+});
+
+export default base;
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <h2>Not Found!111!!</h2>
+    )
+  }
+}
+
+export default NotFound;
